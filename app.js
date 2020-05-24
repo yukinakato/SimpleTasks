d49222d35e5153357d450cc27a0565d1559ca072
Yuki Nakato <crystal.icicle@gmail.com>
2020-05-24 18:22:25 +0900

    fixed sql connection issue

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mysql = require('mysql')
-const dotenv = require('dotenv').config()
+require('dotenv').config()
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-const sql = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -14,26 +14,30 @@ const sql = mysql.createConnection({
 });
 
 // initialize database table
-sql.query('drop table if exists tasks');
-sql.query(' \
-  create table tasks ( \
-    id int unsigned auto_increment primary key, \
-    name text not null, \
-    priority int not null, \
-    completed bit default 0 not null \
-  )'
-);
-sql.query('insert into tasks (name, priority) values ("メモ欄", 0)');
-sql.query('insert into tasks (name, priority) values ("買い物に行く", 3)');
-sql.query('insert into tasks (name, priority) values ("洗濯する", 3)');
-sql.query('insert into tasks (name, priority) values ("部屋の掃除", 3)');
-sql.query('insert into tasks (name, priority) values ("郵便局へ行く", 2)');
-sql.query('insert into tasks (name, priority) values ("知人に電話する", 2)');
-sql.query('insert into tasks (name, priority) values ("本屋へ行く", 1)');
-
+pool.getConnection((err, connection) => {
+  connection.query('start transaction');
+  connection.query('drop table if exists tasks');
+  connection.query(' \
+    create table tasks ( \
+      id int unsigned auto_increment primary key, \
+      name text not null, \
+      priority int not null, \
+      completed bit default 0 not null \
+    )'
+  );
+  connection.query('insert into tasks (name, priority) values ("メモ欄", 0)');
+  connection.query('insert into tasks (name, priority) values ("買い物に行く", 3)');
+  connection.query('insert into tasks (name, priority) values ("洗濯する", 3)');
+  connection.query('insert into tasks (name, priority) values ("部屋の掃除", 3)');
+  connection.query('insert into tasks (name, priority) values ("郵便局へ行く", 2)');
+  connection.query('insert into tasks (name, priority) values ("知人に電話する", 2)');
+  connection.query('insert into tasks (name, priority) values ("本屋へ行く", 1)');
+  connection.query('commit');
+  connection.release();
+});
 
 app.get('/', (req, res) => {
-  sql.query(
+  pool.query(
     'select * from tasks',
     (error, results) => {
       res.render('top.ejs', { tasks: results });
@@ -46,7 +50,7 @@ app.get('/new', (req, res) => {
 });
 
 app.post('/create', (req, res) => {
-  sql.query(
+  pool.query(
     'insert into tasks (name, priority) values (?, ?)',
     [req.body.input_task, req.query.p],
     (error, results) => {
@@ -56,7 +60,7 @@ app.post('/create', (req, res) => {
 });
 
 app.get('/:id/edit/', (req, res) => {
-  sql.query(
+  pool.query(
     'select * from tasks where id = ?',
     [req.params.id],
     (error, results) => {
@@ -69,12 +73,12 @@ app.get('/:id/edit/', (req, res) => {
 });
 
 app.post('/:id/update', (req, res) => {
-  sql.query(
+  pool.query(
     'update tasks set name = ? where id = ?',
     [req.body.input_task, req.params.id],
     (error, results) => {
       if (results.affectedRows === 0)  // if not exist when submit pressed
-        sql.query(
+        pool.query(
           'insert into tasks (name, priority) values (?, ?)',
           [req.body.input_task, req.query.p],
           (error, results) => {
@@ -90,7 +94,7 @@ app.post('/:id/update', (req, res) => {
 
 
 app.post('/:id/delete', (req, res) => {
-  sql.query(
+  pool.query(
     'delete from tasks where id = ?',
     [req.params.id],
     (error, results) => {
@@ -100,19 +104,22 @@ app.post('/:id/delete', (req, res) => {
 });
 
 app.get('/reset', (req, res) => {
-  sql.query('start transaction');
-  sql.query('delete from tasks');
-  sql.query('alter table tasks auto_increment=1');
+  pool.getConnection((err, connection) => {
+    connection.query('start transaction');
+    connection.query('delete from tasks');
+    connection.query('alter table tasks auto_increment=1');
 
-  sql.query('insert into tasks (name, priority) values ("買い物に行く", 3)');
-  sql.query('insert into tasks (name, priority) values ("洗濯する", 3)');
-  sql.query('insert into tasks (name, priority) values ("部屋の掃除", 3)');
-  sql.query('insert into tasks (name, priority) values ("郵便局へ行く", 2)');
-  sql.query('insert into tasks (name, priority) values ("知人に電話する", 2)');
-  sql.query('insert into tasks (name, priority) values ("本屋へ行く", 1)');
-  sql.query('insert into tasks (name, priority) values ("メモ欄", 0)');
+    connection.query('insert into tasks (name, priority) values ("買い物に行く", 3)');
+    connection.query('insert into tasks (name, priority) values ("洗濯する", 3)');
+    connection.query('insert into tasks (name, priority) values ("部屋の掃除", 3)');
+    connection.query('insert into tasks (name, priority) values ("郵便局へ行く", 2)');
+    connection.query('insert into tasks (name, priority) values ("知人に電話する", 2)');
+    connection.query('insert into tasks (name, priority) values ("本屋へ行く", 1)');
+    connection.query('insert into tasks (name, priority) values ("メモ欄", 0)');
 
-  sql.query('commit');
+    connection.query('commit');
+    connection.release();
+  });
   res.redirect('/');
 });
 
